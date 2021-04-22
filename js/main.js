0a20277c9da27986a0e1ccb0348f4023f08d1144
Web time <70425331+WebTimeGit@users.noreply.github.com>
2021-04-22 21:30:03 +0300

    Update clock

--- js/main.js
+++ js/main.js
@@ -1,49 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    // ACCORDION INITIALIZATION START
-    let containerAcc = document.querySelector('.accWrapper')
-    let titleAcc = document.querySelectorAll('.accTitle')
-    if (containerAcc && titleAcc) {
-        for (let i = 0; i < titleAcc.length; i++) {
-            titleAcc[i].addEventListener('click', function () {
-                if (containerAcc.classList.contains('single')) {
-                    titleAcc.forEach(accItem => {
-                        accItem.classList.remove('active')
-                        accItem.nextElementSibling.classList.remove('open')
-                    })
-                    this.classList.add('active')
-                    this.nextElementSibling.classList.add('open')
-                } else if (this.classList.contains('active')) {
-                    this.classList.remove('active')
-                    this.nextElementSibling.classList.remove('open')
-                } else {
-                    this.classList.toggle('active')
-                    this.nextElementSibling.classList.toggle('open')
-                }
-            })
-        }
-    }
-    // ACCORDION INITIALIZATION END
-
-
-    // RESET PLACEHOLDER INPUT ON CLICK START
-    let remove_placeholder = document.querySelectorAll('.remove_placeholder')
-    if (remove_placeholder) {
-        for (let i = 0; i < remove_placeholder.length; i++) {
-            remove_placeholder[i].addEventListener('click', function () {
-                let thisElement = this
-
-                let savePlaceholder = this.getAttribute('placeholder')
-
-                this.setAttribute('placeholder', ' ')
-                document.addEventListener('mouseup', function () {
-                    thisElement.setAttribute('placeholder', savePlaceholder)
-                })
-            })
-        }
-    }
-    // RESET PLACEHOLDER INPUT ON CLICK END
-
     //SCROLL TO TOP
     let toTopBtn = document.querySelector('#scroll_top')
     if (toTopBtn) {
@@ -70,11 +26,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // Start clock  script
     let arrow_hh = document.querySelector('#arrow_hh')
     let arrow_mm = document.querySelector('#arrow_mm')
-    let digital_clock__box = document.querySelectorAll('.digital_clock__box')
-
-    let currentTimeBtn = document.querySelector('#currentTimeBtn')
-    let customTimeBtn = document.querySelector('#customTimeBtn')
-    let removeEventBtn = document.querySelector('#removeEvent')
 
     let currentTime;
     let customTime;
@@ -85,6 +36,48 @@ document.addEventListener("DOMContentLoaded", function () {
     let customHh
     let customMm
     let customSec = 0
+
+    // Функция находит и удаляет с массива все не нужные индексы
+    const findAndRemoveAllItemArr = (arr, value) => {
+        let i = 0;
+        while (i < arr.length) {
+            if (arr[i] === value) {
+                arr.splice(i, 1)
+            } else {
+                ++i
+            }
+        }
+        return arr
+    }
+
+
+    // Функция получаем и устанавливаем текущее время
+    const currentTimeFunction = () => {
+        getCustomTimeArr = []
+        let day = new Date()
+        let getHh = day.getHours()
+        let getMm = day.getMinutes()
+
+        let hh = getHh * 30
+        let mm = (getHh * 360) + (getMm * deg)
+
+        arrow_hh.style.transform = `rotateZ(${(hh) + ((getMm * deg) / 12)}deg)`
+        arrow_mm.style.transform = `rotateZ(${mm}deg)`
+
+        let timeString = day.toLocaleTimeString('en-GB');
+        let newTimeArr = timeString.split('')
+        console.log('текущее время ' + timeString.substr(0, 5))
+        findAndRemoveAllItemArr(newTimeArr, ':')
+
+        customTimeArr.forEach((el, i) => {
+            el.value = newTimeArr[i]
+            getCustomTimeArr.push(el.value)
+        })
+    }
+
+    currentTime = setInterval(currentTimeFunction, 1000)
+
+
 
     // Получаем массив с полей input времени + мини проверка
     const getTimeArrFunction = () => {
@@ -101,6 +94,7 @@ document.addEventListener("DOMContentLoaded", function () {
             customMm = Number(`${getCustomTimeArr[2]}${getCustomTimeArr[3]}`)
         })
     }
+
 
     //Валидация input ввода времени
     customTimeArr.forEach(el => {
@@ -122,49 +116,13 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     })
 
-    // Функция находит и удаляет с массива все не нужные индексы
-    const findAndRemoveAllItemArr = (arr, value) => {
-        let i = 0;
-        while (i < arr.length) {
-            if (arr[i] === value) {
-                arr.splice(i, 1)
-            } else {
-                ++i
-            }
-        }
-        return arr
-    }
 
-    // Функция отменяет все setInterval
-    const removeEvent = () => {
-        clearInterval(currentTime)
-        clearInterval(customTime)
-    }
-
-    // Функция получаем и устанавливаем текущее время
-    const currentTimeFunction = () => {
-        let day = new Date()
-        let getHh = day.getHours()
-        let getMm = day.getMinutes()
-
-        let hh = getHh * 30
-        let mm = (getHh * 360) + (getMm * deg)
-
-        arrow_hh.style.transform = `rotateZ(${(hh) + ((getMm * deg) / 12)}deg)`
-        arrow_mm.style.transform = `rotateZ(${mm}deg)`
-
-        let timeString = day.toLocaleTimeString('en-GB');
-        let newTimeArr = timeString.split('')
-        console.log('текущее время ' + timeString.substr(0, 5))
-        findAndRemoveAllItemArr(newTimeArr, ':')
-
-        digital_clock__box.forEach((el, i) => {
-            el.textContent = newTimeArr[i]
-        })
-    }
-    currentTimeFunction()
     // Функция получаем и устанавливаем кастомное время
     const customTimeFunction = () => {
+
+        getCustomTimeArr = []
+        getTimeArrFunction()
+
         let getHh = customHh
         let getMm = customMm
         customSec++
@@ -182,16 +140,14 @@ document.addEventListener("DOMContentLoaded", function () {
         let timeString = `${getHh}${getMm}`;
         console.log('кастомные часы ' + timeString.match(/.{1,2}/g).join(":"))
         let newTimeArr = timeString.split('')
-        digital_clock__box.forEach((el, i) => {
-            el.textContent = newTimeArr[i]
+        customTimeArr.forEach((el, i) => {
+            el.value = newTimeArr[i]
         })
 
         if (customSec === 60) {
             console.log('+1 minute')
             customMm += 1
             customSec = 0
-            // arrow_hh.style.transition = `1s ease`
-            // arrow_mm.style.transition = `1s ease`
             console.log(customMm)
             console.log(customSec)
         } else if (getMm === 60) {
@@ -203,31 +159,16 @@ document.addEventListener("DOMContentLoaded", function () {
             customHh = 0
             customMm = 0
             customSec = 0
-            // arrow_hh.style.transition = `initial`
-            // arrow_mm.style.transition = `initial`
         }
     }
 
-    // Кнопка активации текущего времени
-    currentTimeBtn.addEventListener('click', () => {
-        removeEvent()
-        currentTime = setInterval(currentTimeFunction, 1000)
-    })
+    customTimeArr.forEach((el, i) => {
+        el.addEventListener('keydown', ()=>{
+            clearInterval(currentTime)
+            clearInterval(customTime)
 
-    // Кнопка активации кастомного времени
-    customTimeBtn.addEventListener('click', () => {
-        removeEvent()
-        getCustomTimeArr = []
-        getTimeArrFunction()
-        customTime = setInterval(customTimeFunction, 200)
-    })
-
-    // кнопка отменяет все setInterval
-    removeEventBtn.addEventListener('click', () => {
-        removeEvent()
-        getCustomTimeArr = []
-        customSec = 0
-        console.log('STOP')
+            customTime = setInterval(customTimeFunction, 1000)
+        })
     })
 
 });
